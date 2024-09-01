4b2308629640a42d97e93e42532d4cb7182af57c
Davidaprilio <david1.aprilio4@gmail.com>
2024-09-01 21:43:04 +0700

    update(): modify view Edit profile

--- resources/js/Pages/Profile/Partials/DeleteUserForm.tsx
+++ resources/js/Pages/Profile/Partials/DeleteUserForm.tsx
@@ -46,25 +46,23 @@ export default function DeleteUserForm({ className = '' }: { className?: string 
     return (
         <section className={`space-y-6 ${className}`}>
             <header>
-                <h2 className="text-lg font-medium text-gray-900 dark:text-gray-100">Delete Account</h2>
+                <h2 className="text-lg font-medium text-gray-900 dark:text-gray-100">Hapus Akun</h2>
 
                 <p className="mt-1 text-sm text-gray-600 dark:text-gray-400">
-                    Once your account is deleted, all of its resources and data will be permanently deleted. Before
-                    deleting your account, please download any data or information that you wish to retain.
+                    Aksi ini akan menghapus akun anda, dan samua data anda akan dihapus secara permanen. Sebelum menghapus akun harap mengunduh data anda atau informasi yang mungkin penting.
                 </p>
             </header>
 
-            <DangerButton onClick={confirmUserDeletion}>Delete Account</DangerButton>
+            <DangerButton onClick={confirmUserDeletion}>Hapus Akun</DangerButton>
 
             <Modal show={confirmingUserDeletion} onClose={closeModal}>
                 <form onSubmit={deleteUser} className="p-6">
                     <h2 className="text-lg font-medium text-gray-900 dark:text-gray-100">
-                        Are you sure you want to delete your account?
+                        Yakin untuk menghapus akun?
                     </h2>
 
                     <p className="mt-1 text-sm text-gray-600 dark:text-gray-400">
-                        Once your account is deleted, all of its resources and data will be permanently deleted. Please
-                        enter your password to confirm you would like to permanently delete your account.
+                        Aksi ini akan menghapus akun anda, dan samua data anda akan dihapus secara permanen. Mohon konfirmasi tindakan ini.
                     </p>
 
                     <div className="mt-6">
@@ -89,7 +87,7 @@ export default function DeleteUserForm({ className = '' }: { className?: string 
                         <SecondaryButton onClick={closeModal}>Cancel</SecondaryButton>
 
                         <DangerButton className="ms-3" disabled={processing}>
-                            Delete Account
+                            Hapus Akun Saya
                         </DangerButton>
                     </div>
                 </form>
